9525182ff962a93d6bbe52a383e6304fa02862d3
Om Patel <84760165+ompatel585@users.noreply.github.com>
2025-08-01 17:34:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,29 +33,9 @@ function LoadingScreen() {
         loading="eager"
       />
 {/*       <FaSpinner className="animate-spin text-4xl text-indigo-600 mb-3" /> */}
-      <div className="relative w-12 h-12 mb-5">
-  <div className="absolute inset-0 rounded-full border-t-4 border-b-4 border-indigo-500 animate-spin"></div>
-  <div className="absolute inset-1 rounded-full border-t-4 border-b-4 border-indigo-200 animate-spin opacity-50"></div>
-</div>
-
-
-
-      <div className="relative w-10 h-10 mb-5">
-  <div className="absolute top-0 left-1/2 w-2 h-2 bg-purple-500 rounded-full animate-[spin_1.2s_linear_infinite] origin-[0%_200%]"></div>
-  <div className="absolute top-0 left-1/2 w-2 h-2 bg-blue-400 rounded-full animate-[spin_2s_linear_infinite] origin-[0%_250%] delay-200"></div>
-</div>
-
-
-
-
-      <div className="relative w-12 h-12 mb-5">
-  <div className="absolute w-3 h-3 rounded-full bg-indigo-500 animate-ping top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
-  <div className="absolute top-0 left-1/2 w-2 h-2 bg-indigo-400 rounded-full animate-[spin_2s_linear_infinite] origin-center"></div>
-</div>
-
       
 
-        
+      
 
   );
 }
